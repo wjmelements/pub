f14b2b57bbbd9c3a508bd2871907f29e1bec91ff
William Morriss <wjmelements@gmail.com>
2017-11-10 16:53:04 -0800

    onChangeTitle

--- grafiti/client/lib/paths/submit.js
+++ grafiti/client/lib/paths/submit.js
@@ -11,10 +11,10 @@ Submit = {
 var instance_title;
 var instance_content;
 var instance_preview;
+var instance_preview_title;
 function clearSubmission() {
     instance_title.value = '';
     instance_content.value = '';
-    instance_preview.innerHTML = '';
 }
 function showTransactionHash(hash) {
     document.getElementById('onSuccess').hidden = false;
@@ -25,10 +25,19 @@ Template.submit.onRendered(function () {
     instance_title = document.getElementById('submit-title');
     instance_content = document.getElementById('submit-content');
     instance_preview = document.getElementById('submit-preview');
+    instance_preview_title = document.getElementById('submit-preview-title');
 });
 
 function onChange() {
     instance_preview.innerHTML = instance_content.value;
+}
+
+function onChangeTitle() {
+    var title = instance_title.value;
+    if (title.length == 0) {
+        title = "Preview";
+    }
+    instance_preview_title.innerHTML = title;
 }
 
 Template.submit.events({
@@ -50,5 +59,11 @@ Template.submit.events({
     },
     'change textarea#submit-content'(event) {
         onChange();
+    },
+    'keyup #submit-title'(event) {
+        onChangeTitle();
+    },
+    'change #submit-title'(event) {
+        onChangeTitle();
     },
 });
